b33fba3acde25c20c831cdaab56b074742ff358b
Kevin Ghobrial <109962480+Kevin-Ghobrial@users.noreply.github.com>
2026-09-15 17:32:17 -0400

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()){
+            return false;
+        }
+
+        int len = s.size();
+
+        unordered_map <char, int> s_map;
+        unordered_map <char, int> t_map;
+
+        for(int i = 0; i < len; i++){
+            s_map[s[i]]++;
+            t_map[t[i]]++;
+        }
+        
+        //bool isAna = s_map == t_map ? true : false;
+        
+        return s_map == t_map ? true : false;
+    }
+};
